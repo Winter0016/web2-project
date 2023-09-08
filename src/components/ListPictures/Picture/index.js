7da7ca53a86302955118ca21ae953899ae097e7d
Winter0016 <chauquangphuc2604.2604@gmail.com>
2023-09-08 22:11:23 +0700

    picturelist finished

--- src/components/ListPictures/Picture/index.js
+++ src/components/ListPictures/Picture/index.js
@@ -4,13 +4,11 @@ const Picture = (props) => {
     const { data } = props;
     console.log('HLLLO')
     return (
-    <div className="container3">
-        <div className="box1">
-            <div className="p1">
-                <img src={data.image}/>
-                <div className="font">
-                    {data.description}Read more
-                </div>
+    <div className="pfirst">
+        <div className="psecond">
+            <img src={data.image}/>
+            <div className="font">
+                {data.description}Read more
             </div>
         </div>
     </div>
--- src/components/ListPictures/index.js
+++ src/components/ListPictures/index.js
@@ -3,11 +3,11 @@ import Pagination from "../Pagination";
 import { changePage } from "../../container/Patron/actions";
 import { useDispatch, useSelector, Provider } from "react-redux";
 import { getListPictureApi } from "../../api/Pictures";
-
 const pictures = await getListPictureApi();
 const pages = pictures.pages
 const myData = pictures.data
 
+import ("./list.css")
 const ListPictures = () => {
     const changePageHandler = (value) => {
         changePage(value);
@@ -16,24 +16,12 @@ const ListPictures = () => {
     //const { pictures, pages } = useSelector((state) => state.PictureReducer);
 
     return (
-        <div>
-            <div className="blogs2">
+        <div className="BLOG2">
                 {myData.map((item) => (
                     <Picture
                         data = {item}
                     />
                 ))}    
-            </div>
-
-{/*            <div className="pagination">
-                <Pagination
-                    currentPage = {pages.current}
-                    totalCount = {pages.total * 5}
-                    pageSize={5}
-                    onPageChange={(page) => changePageHandler(page)}
-                />
-            </div>
-*/}
         </div>
     );
 };
